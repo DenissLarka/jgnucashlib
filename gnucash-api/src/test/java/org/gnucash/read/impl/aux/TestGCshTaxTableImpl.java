5e264161ae215b5845b122e5cc623a324162c872
James Rossini <james.rossini@posteo.de>
2023-10-05 10:37:03 +0200

    Read-branch: More test cases and test data for GCshTaxTable.

--- gnucash-api/src/test/java/org/gnucash/read/impl/aux/TestGCshTaxTableImpl.java
+++ gnucash-api/src/test/java/org/gnucash/read/impl/aux/TestGCshTaxTableImpl.java
@@ -3,11 +3,13 @@ package org.gnucash.read.impl.aux;
 import static org.junit.Assert.assertEquals;
 
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Collection;
 
 import org.gnucash.ConstTest;
 import org.gnucash.read.GnucashFile;
 import org.gnucash.read.aux.GCshTaxTable;
+import org.gnucash.read.aux.GCshTaxTableEntry;
 import org.gnucash.read.impl.GnucashFileImpl;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,9 +18,18 @@ import junit.framework.JUnit4TestAdapter;
 
 public class TestGCshTaxTableImpl
 {
-  private static final String TAXTABLE_1_ID = "3c9690f9f31b4cd0baa936048b833c06";
-  private static final String TAXTABLE_2_ID = "cba6011c826f426fbc4a1a72c3d6c8ee";
+  // DE
+  // Note the funny parent/child pair.
+  private static final String TAXTABLE_DE_1_1_ID = "3c9690f9f31b4cd0baa936048b833c06"; // DE_USt_Std "parent"
+  private static final String TAXTABLE_DE_1_2_ID = "cba6011c826f426fbc4a1a72c3d6c8ee"; // DE_USt_Std "child"
+  private static final String TAXTABLE_DE_2_ID   = "c518af53a93c4a5cb3e2161b7b358e68"; // DE_USt_red
     
+  // FR
+  private static final String TAXTABLE_FR_1_ID   = "de4c17d1eb0e4f088ba73d4c697032f0"; // FR_USt_Std
+  private static final String TAXTABLE_FR_2_ID   = "e279d5cc81204f1bb6cf672ef3357c0c"; // FR_USt_red
+    
+  private static final String TAX_ACCT_ID        = "1a5b06dada56466197edbd15e64fd425"; // Root Account::Fremdkapital::Steuerverbindl
+
   private GnucashFile  gcshFile = null;
   private GCshTaxTable taxTab = null;
   
@@ -70,9 +81,89 @@ public class TestGCshTaxTableImpl
   {
       Collection<GCshTaxTable> taxTableList = gcshFile.getTaxTables();
       
-      assertEquals(2, taxTableList.size());
+      assertEquals(5, taxTableList.size());
+
+      // ::TODO: Sort array for predictability
+      Object[] taxTableArr = taxTableList.toArray();
       
-      assertEquals(TAXTABLE_1_ID, ((GCshTaxTable) taxTableList.toArray()[0]).getId());
-      assertEquals(TAXTABLE_2_ID, ((GCshTaxTable) taxTableList.toArray()[1]).getId());
+      // funny, this parent/child relationship full of redundancies...
+      assertEquals(TAXTABLE_DE_1_1_ID, ((GCshTaxTable) taxTableArr[1]).getId());
+      assertEquals(TAXTABLE_DE_1_2_ID, ((GCshTaxTable) taxTableArr[4]).getId());
+      
+      // Here, it's what you would expect:
+      assertEquals(TAXTABLE_DE_2_ID, ((GCshTaxTable) taxTableArr[2]).getId());
+      assertEquals(TAXTABLE_FR_1_ID, ((GCshTaxTable) taxTableArr[3]).getId());
+      assertEquals(TAXTABLE_FR_2_ID, ((GCshTaxTable) taxTableArr[0]).getId());
+  }
+
+  @Test
+  public void test02_1() throws Exception
+  {
+      taxTab = gcshFile.getTaxTableByID(TAXTABLE_DE_1_1_ID);
+      
+      assertEquals("DE_USt_Std", taxTab.getName());
+      assertEquals(null, taxTab.getParentID());
+
+      assertEquals(1, taxTab.getEntries().size());
+      assertEquals(19.0, ((GCshTaxTableEntry) taxTab.getEntries().toArray()[0]).getAmount().doubleValue(), ConstTest.DIFF_TOLERANCE );
+      assertEquals(GCshTaxTableEntry.TYPE_PERCENT, ((GCshTaxTableEntry) taxTab.getEntries().toArray()[0]).getType() );
+      assertEquals(TAX_ACCT_ID, ((GCshTaxTableEntry) taxTab.getEntries().toArray()[0]).getAccountID() );
+  }
+
+  @Test
+  public void test02_2() throws Exception
+  {
+      taxTab = gcshFile.getTaxTableByID(TAXTABLE_DE_1_2_ID);
+      
+      assertEquals("USt_Std", taxTab.getName()); // sic, old name w/o prefix "DE_"
+      assertEquals(TAXTABLE_DE_1_1_ID, taxTab.getParentID());
+
+      assertEquals(1, taxTab.getEntries().size());
+      assertEquals(19.0, ((GCshTaxTableEntry) taxTab.getEntries().toArray()[0]).getAmount().doubleValue(), ConstTest.DIFF_TOLERANCE );
+      assertEquals(GCshTaxTableEntry.TYPE_PERCENT, ((GCshTaxTableEntry) taxTab.getEntries().toArray()[0]).getType() );
+      assertEquals(TAX_ACCT_ID, ((GCshTaxTableEntry) taxTab.getEntries().toArray()[0]).getAccountID() );
+  }
+
+  @Test
+  public void test03() throws Exception
+  {
+      taxTab = gcshFile.getTaxTableByID(TAXTABLE_DE_2_ID);
+      
+      assertEquals("DE_USt_red", taxTab.getName());
+      assertEquals(null, taxTab.getParentID());
+
+      assertEquals(1, taxTab.getEntries().size());
+      assertEquals(7.0, ((GCshTaxTableEntry) taxTab.getEntries().toArray()[0]).getAmount().doubleValue(), ConstTest.DIFF_TOLERANCE );
+      assertEquals(GCshTaxTableEntry.TYPE_PERCENT, ((GCshTaxTableEntry) taxTab.getEntries().toArray()[0]).getType() );
+      assertEquals(TAX_ACCT_ID, ((GCshTaxTableEntry) taxTab.getEntries().toArray()[0]).getAccountID() );
+  }
+
+
+  @Test
+  public void test04() throws Exception
+  {
+      taxTab = gcshFile.getTaxTableByID(TAXTABLE_FR_1_ID);
+      
+      assertEquals("FR_USt_Std", taxTab.getName());
+      assertEquals(null, taxTab.getParentID());
+
+      assertEquals(1, taxTab.getEntries().size());
+      assertEquals(20.0, ((GCshTaxTableEntry) taxTab.getEntries().toArray()[0]).getAmount().doubleValue(), ConstTest.DIFF_TOLERANCE );
+      assertEquals(GCshTaxTableEntry.TYPE_PERCENT, ((GCshTaxTableEntry) taxTab.getEntries().toArray()[0]).getType() );
+      assertEquals(TAX_ACCT_ID, ((GCshTaxTableEntry) taxTab.getEntries().toArray()[0]).getAccountID() );
+  }
+
+  @Test
+  public void test05() throws Exception
+  {
+      taxTab = gcshFile.getTaxTableByID(TAXTABLE_FR_2_ID);
+      
+      assertEquals("FR_USt_red", taxTab.getName());
+      assertEquals(null, taxTab.getParentID());
+
+      assertEquals(1, taxTab.getEntries().size());
+      assertEquals(10.0, ((GCshTaxTableEntry) taxTab.getEntries().toArray()[0]).getAmount().doubleValue(), ConstTest.DIFF_TOLERANCE );
+      assertEquals(GCshTaxTableEntry.TYPE_PERCENT, ((GCshTaxTableEntry) taxTab.getEntries().toArray()[0]).getType() );
+      assertEquals(TAX_ACCT_ID, ((GCshTaxTableEntry) taxTab.getEntries().toArray()[0]).getAccountID() );
   }
 }
